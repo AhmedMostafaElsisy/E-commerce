cced6247a71259ac95d39ec72cebace9d82ebb16
ahmed <ahmedmostafacc@gmail.com>
2022-12-14 18:21:16 +0200

    CO-15 CO-59 add bottom nav to route generator , route name

--- lib/Presentation/Routes/route_generator.dart
+++ lib/Presentation/Routes/route_generator.dart
@@ -5,6 +5,7 @@ import '../../features/Auth_feature/Presentation/screens/login_home_page.dart';
 import '../../features/Auth_feature/Presentation/screens/sign_up_screen.dart';
 import '../../features/Auth_feature/Presentation/screens/verification_code_screen.dart';
 import '../../features/Auth_feature/Presentation/screens/forget_password_screen.dart';
+import '../../features/Home_feature/presentation/Bottom_Nav_Screen/bottom_nav_bar.dart';
 import '../../features/Home_feature/presentation/Home_Screens/home_page.dart';
 import '../Screens/Forget_Password_Screen/new_password_screen.dart';
 import '../Screens/Notification_Screen/notification_screen.dart';
@@ -39,6 +40,9 @@ class RouteGenerator {
       case RouteNames.notificationPageRoute:
         return RouteAnimation().animationFromCenterRightToCenterLeft(
             page: const NotificationListScreen());
+      case RouteNames.mainBottomNavPageRoute:
+        return RouteAnimation()
+            .animationFromCenterRightToCenterLeft(page: const BottomNavBar());
 
       default:
         return MaterialPageRoute(
