4d677fe9058bf2290d9b18d9089d0ce2e77b356e
ahmed <ahmedmostafacc@gmail.com>
2022-12-19 15:55:49 +0200

    CO-17 CO-86 create request list history logic

--- lib/features/Home_feature/presentation/logic/request_cubit/request_cubit.dart
+++ lib/features/Home_feature/presentation/logic/request_cubit/request_cubit.dart
@@ -1,5 +1,5 @@
 import 'package:captien_omda_customer/features/Home_feature/Domain/enitiy/request_model.dart';
-
+import 'package:flutter/cupertino.dart';
 import '../../../Domain/use_cases/request_use_case.dart';
 import 'request_cubit_states.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,7 +9,14 @@ class RequestCubit extends Cubit<RequestCubitState> {
   final RequestUesCases _requestUesCase;
 
   static RequestCubit get(context) => BlocProvider.of(context);
-List<RequestModel> requestList=[];
+  List<RequestModel> requestList = [];
+  List<RequestModel> requestHistoryList = [];
+
+  ///pagination
+  int page = 1;
+  late ScrollController scrollController;
+  bool hasMoreData = false;
+
   ///get home screen requests
   getHomeRequest() async {
     emit(RequestHomeLoadingState());
@@ -21,8 +28,54 @@ List<RequestModel> requestList=[];
         if (requestList.isEmpty) {
           emit(RequestHomeEmptyState());
         } else {
-          this.requestList=requestList;
+          this.requestList = requestList;
           emit(RequestHomeSuccessState(requestList));
+        }
+      },
+    );
+  }
+
+  void setupScrollController() {
+    if (scrollController.offset >
+            scrollController.position.maxScrollExtent - 200 &&
+        scrollController.offset <= scrollController.position.maxScrollExtent) {
+      if (state is! RequestMoreDataHistoryLoadingState && hasMoreData) {
+        whenScrollHistoryPagination();
+      }
+    }
+  }
+
+  ///get request history pagination
+  whenScrollHistoryPagination() async {
+    emit(RequestMoreDataHistoryLoadingState());
+    page = page + 1;
+    var result = await _requestUesCase.callHistoryRequest(page: page);
+
+    result.fold(
+      (failure) => emit(RequestMoreHistoryFailedState(failure)),
+      (requestList) {
+        hasMoreData = requestList.length == 10;
+        requestHistoryList.addAll(requestList);
+        emit(RequestHistorySuccessState());
+      },
+    );
+  }
+
+  ///get request history list
+  getRequestHistory() async {
+    emit(RequestHistoryLoadingState());
+    page = 1;
+    var result = await _requestUesCase.callHistoryRequest(page: page);
+
+    result.fold(
+      (failure) => emit(RequestHistoryFailedState(failure)),
+      (requestList) {
+        hasMoreData = requestList.length == 10;
+        if (requestList.isEmpty) {
+          emit(RequestHistoryEmptyState());
+        } else {
+          requestHistoryList = requestList;
+          emit(RequestHistorySuccessState());
         }
       },
     );
