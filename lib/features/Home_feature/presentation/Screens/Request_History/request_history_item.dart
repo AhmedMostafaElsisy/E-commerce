caeab71455b7c21f271236c200f736a7a3d5e46a
ahmed <ahmedmostafacc@gmail.com>
2022-12-19 15:55:33 +0200

    CO-17 CO-85 create request list history ui

---
+++ lib/features/Home_feature/presentation/Screens/Request_History/request_history_item.dart
@@ -0,0 +1,154 @@
+import 'package:captien_omda_customer/features/Home_feature/Domain/enitiy/request_model.dart';
+import 'package:flutter/material.dart';
+import 'package:captien_omda_customer/core/Helpers/shared.dart';
+import 'package:captien_omda_customer/core/presentation/Widgets/common_cached_image_widget.dart';
+import 'package:captien_omda_customer/core/presentation/Widgets/common_title_text.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import '../../../../../core/Constants/app_constants.dart';
+import '../../../../../core/Helpers/shared_texts.dart';
+import '../../../../../core/presentation/Widgets/common_asset_svg_image_widget.dart';
+
+class RequestHistoryItem extends StatelessWidget {
+  final RequestModel model;
+
+  const RequestHistoryItem({Key? key, required this.model}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(3),
+      width: SharedText.screenWidth,
+      decoration: BoxDecoration(
+          color: AppConstants.lightWhiteColor,
+          boxShadow: [
+            BoxShadow(
+                color: AppConstants.lightBlackColor.withOpacity(0.25),
+                blurRadius: 4.0,
+                offset: const Offset(0, 0))
+          ],
+          borderRadius:
+              BorderRadius.circular(AppConstants.containerBorderRadius)),
+      padding: const EdgeInsets.all(AppConstants.pagePadding),
+      child: Column(children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                commonCachedImageWidget(context, "imageUrl",
+                    height: 38, width: 38, isCircular: true, isProfile: true),
+                getSpaceWidth(AppConstants.smallPadding),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const CommonTitleText(
+                      textKey: "Drivier name",
+                      textFontSize: AppConstants.smallFontSize,
+                      textColor: AppConstants.lightBlackColor,
+                      textWeight: FontWeight.w600,
+                    ),
+                    getSpaceHeight(AppConstants.smallPadding / 2),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        CommonTitleText(
+                          textKey: "4.3",
+                          textFontSize: AppConstants.smallFontSize - 2,
+                          textColor: AppConstants.mainTextColor,
+                          textWeight: FontWeight.w500,
+                        ),
+                        CommonAssetSvgImageWidget(
+                          imageString: "rate_star.svg",
+                          width: 16,
+                          height: 16,
+                          fit: BoxFit.cover,
+                          imageColor: AppConstants.starRatingColor,
+                        ),
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const CommonAssetSvgImageWidget(
+                    imageString: "cash_icon.svg",
+                    height: 25,
+                    width: 25,
+                    fit: BoxFit.contain,
+                    imageColor: AppConstants.lightBorderColor,
+                  ),
+                  getSpaceWidth(AppConstants.smallPadding / 2),
+                  CommonTitleText(
+                    textKey: "${model.price!} ${AppLocalizations.of(context)!.lblEGP}",
+                    textColor: AppConstants.lightBorderColor,
+                    textWeight: FontWeight.w700,
+                    textFontSize: AppConstants.normalFontSize,
+                  ),
+                ]),
+          ],
+        ),
+
+        ///spacer
+        getSpaceHeight(AppConstants.smallPadding),
+
+        ///spacer
+        const Divider(
+          color: AppConstants.lightGrayColor,
+          thickness: 1,
+        ),
+
+        ///spacer
+        getSpaceHeight(AppConstants.smallPadding),
+        Row(
+          children: [
+            const CommonAssetSvgImageWidget(
+                imageString: "current_location.svg",
+                fit: BoxFit.contain,
+                height: 24,
+                width: 24),
+            getSpaceWidth(AppConstants.smallPadding),
+            SizedBox(
+              width: getWidgetWidth(250),
+              child:  CommonTitleText(
+                textKey:model.fromLocation!.locationName!,
+                textColor: AppConstants.lightBlackColor,
+                textWeight: FontWeight.w600,
+                textFontSize: AppConstants.normalFontSize,
+                minTextFontSize: AppConstants.normalFontSize,
+                textAlignment: TextAlign.start,
+              ),
+            ),
+          ],
+        ),
+
+        ///spacer
+        getSpaceHeight(AppConstants.pagePadding - 4),
+        Row(
+          children: [
+            const CommonAssetSvgImageWidget(
+                imageString: "to_location.svg",
+                fit: BoxFit.contain,
+                height: 24,
+                width: 24),
+            getSpaceWidth(AppConstants.smallPadding),
+            SizedBox(
+              width: getWidgetWidth(250),
+              child:  CommonTitleText(
+                textKey:model.toLocation!.locationName!,
+                textColor: AppConstants.lightBlackColor,
+                textWeight: FontWeight.w600,
+                textFontSize: AppConstants.normalFontSize,
+                minTextFontSize: AppConstants.normalFontSize,
+                textAlignment: TextAlign.start,
+              ),
+            ),
+          ],
+        ),
+      ]),
+    );
+  }
+}
