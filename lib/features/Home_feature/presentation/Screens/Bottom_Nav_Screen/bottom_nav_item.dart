b33d4078e15fc0b15e258068e1ed4be561bb3b91
queentechdeveloper <mahmoudragab0809@gmail.com>
2022-12-26 07:00:00 +0200

    edit item of bottom_nav_item.dart to make it fit new design

--- lib/features/Home_feature/presentation/Screens/Bottom_Nav_Screen/bottom_nav_item.dart
+++ lib/features/Home_feature/presentation/Screens/Bottom_Nav_Screen/bottom_nav_item.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+
 import '../../../../../core/Constants/app_constants.dart';
 import '../../../../../core/Helpers/shared.dart';
 import '../../../../../core/presentation/Widgets/common_asset_svg_image_widget.dart';
 import '../../../../../core/presentation/Widgets/common_title_text.dart';
-
 
 class BottomBarItem extends StatelessWidget {
   final String title;
@@ -20,22 +20,25 @@ class BottomBarItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: getWidgetWidth(120),
-      height: getWidgetHeight(40),
+      width: getWidgetWidth(48),
+      height: getWidgetHeight(50),
       decoration: BoxDecoration(
-          color: isSelected? AppConstants.mainColor:AppConstants.lightWhiteColor,
-          borderRadius:
-              BorderRadius.circular(AppConstants.bottomSheetBorderRadius)),
-      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        if (isSelected)
-          CommonAssetSvgImageWidget(imageString: image, height: 24, width: 24),
-        getSpaceWidth(AppConstants.smallPadding),
+          color: AppConstants.lightWhiteColor,
+          borderRadius: BorderRadius.circular(AppConstants.smallBorderRadius)),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        CommonAssetSvgImageWidget(
+          imageString: image,
+          height: 24,
+          width: 24,
+          imageColor:
+              isSelected ? AppConstants.mainColor : AppConstants.lightGreyColor,
+        ),
+        getSpaceHeight(AppConstants.smallPadding),
         CommonTitleText(
           textKey: title,
-          textWeight: isSelected ? FontWeight.w700 : FontWeight.w600,
-          textColor: isSelected
-              ? AppConstants.lightWhiteColor
-              : AppConstants.lightBlackColor,
+          textWeight: FontWeight.w600,
+          textColor:
+              isSelected ? AppConstants.mainColor : AppConstants.lightGreyColor,
           textFontSize: AppConstants.smallFontSize,
         )
       ]),
