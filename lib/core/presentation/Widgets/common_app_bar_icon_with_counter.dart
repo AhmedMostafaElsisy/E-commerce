2850b47ee315942c4bc5e1b89a7a16d6febb7f1d
queentechdeveloper <mahmoudragab0809@gmail.com>
2022-12-31 23:35:03 +0200

    add new widget common_app_bar_icon_with_counter.dart to use it on appbar of home screen

---
+++ lib/core/presentation/Widgets/common_app_bar_icon_with_counter.dart
@@ -0,0 +1,69 @@
+import 'package:captien_omda_customer/core/Constants/app_constants.dart';
+import 'package:captien_omda_customer/core/Helpers/shared.dart';
+import 'package:captien_omda_customer/core/presentation/Widgets/common_asset_svg_image_widget.dart';
+import 'package:captien_omda_customer/core/presentation/Widgets/common_title_text.dart';
+import 'package:flutter/material.dart';
+
+import '../../Helpers/shared_texts.dart';
+
+class CommonAppBarImageWithCounter extends StatefulWidget {
+  final String imagePath;
+  final int itemCounter;
+  final bool withCounter;
+
+  const CommonAppBarImageWithCounter(
+      {Key? key,
+      required this.imagePath,
+      this.itemCounter = 0,
+      this.withCounter = false})
+      : super(key: key);
+
+  @override
+  State<CommonAppBarImageWithCounter> createState() =>
+      _CommonAppBarImageWithCounterState();
+}
+
+class _CommonAppBarImageWithCounterState
+    extends State<CommonAppBarImageWithCounter> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: getWidgetWidth(36),
+      height: getWidgetHeight(36),
+      child: Center(
+        child: Stack(
+          children: [
+            Positioned(
+              bottom: 0,
+              child: CommonAssetSvgImageWidget(
+                  imageString: widget.imagePath, height: 22, width: 22),
+            ),
+            if (widget.withCounter)
+              Positioned(
+                top: getWidgetHeight(6),
+                right: SharedText.isAppLocalAr() ? null : 3,
+                left: SharedText.isAppLocalAr() ? 3 : null,
+                child: Container(
+                  height: getWidgetHeight(16),
+                  width: getWidgetWidth(16),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: AppConstants.lightRedColor,
+                  ),
+                  child: Center(
+                    child: CommonTitleText(
+                      textKey: widget.itemCounter.toString(),
+                      textColor: AppConstants.lightWhiteColor,
+                      textFontSize: AppConstants.xSmallFontSize,
+                      minTextFontSize: AppConstants.xSmallFontSize,
+                      textWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
