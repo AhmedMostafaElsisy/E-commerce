dc88cc07895d49d98f56b736080639e30e7d90ba
ahmed <ahmedmostafacc@gmail.com>
2022-12-31 13:42:03 +0200

    add the full data for shop model

--- lib/core/model/shop_model.dart
+++ lib/core/model/shop_model.dart
@@ -7,15 +7,44 @@ class ShopModel {
   String? image;
   String? location;
   String? category;
+  String? subCategory;
+  String? phone;
+  String? email;
+  String? ownerName;
+  String? city;
+  String? area;
 
-  ShopModel({this.id, this.name, this.image, this.location, this.category});
+  ShopModel(
+      {this.id,
+      this.name,
+      this.image,
+      this.location,
+      this.category,
+      this.subCategory,
+      this.phone,
+      this.email,
+      this.ownerName,
+      this.city,
+      this.area});
 
   factory ShopModel.fromJson(Map<String, dynamic> json) {
     return ShopModel(
-        id: json["id"],
-        name: json["shop_name"] ?? "--",
-        image: json["shop_image"] ?? "",
-        category: json["category"] ?? "--",
-        location: json["location"] ?? "--");
+      id: json["id"],
+      name: json["shop_name"] ?? "--",
+      image: json["shop_image"] ?? "",
+      category: json["category"] ?? "--",
+      location: json["location"] ?? "--",
+      subCategory: json["sub_category"] ?? "--",
+      phone: json["phone"] ?? "--",
+      email: json["email"] ?? "--",
+      ownerName: json["owner_name"] ?? "--",
+      city: json["city"] ?? "--",
+      area: json["area"] ?? "--",
+    );
+  }
+
+  @override
+  String toString() {
+    return 'ShopModel{id: $id, name: $name, image: $image, location: $location, category: $category, subCategory: $subCategory, phone: $phone, email: $email, ownerName: $ownerName, city: $city, area: $area}';
   }
 }
