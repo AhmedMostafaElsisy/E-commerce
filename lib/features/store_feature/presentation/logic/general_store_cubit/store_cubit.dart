f04da6774457ba1cd6a213029efc2f5933a573cd
ahmed <ahmedmostafacc@gmail.com>
2023-01-07 14:20:27 +0200

    move store cubit into folder

--- lib/features/store_feature/presentation/logic/general_store_cubit/store_cubit.dart
+++ lib/features/store_feature/presentation/logic/general_store_cubit/store_cubit.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
-import '../../../../core/model/shop_model.dart';
-import '../../domain/ues_cases/ues_cases.dart';
+import '../../../../../core/model/shop_model.dart';
+import '../../../domain/ues_cases/ues_cases.dart';
 import 'store_states.dart';
 
 class StoreCubit extends Cubit<StoreStates> {
--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -53,7 +53,8 @@ import 'features/store_feature/data/data_scoures/remote_data_scoures.dart';
 import 'features/store_feature/data/repository/my_store_repository.dart';
 import 'features/store_feature/domain/repository/store_repository_interface.dart';
 import 'features/store_feature/domain/ues_cases/ues_cases.dart';
-import 'features/store_feature/presentation/logic/store_cubit.dart';
+import 'features/store_feature/presentation/logic/general_store_cubit/store_cubit.dart';
+import 'features/store_feature/presentation/logic/single_store_cubit/my_store_cubit.dart';
 import 'features/trip_feature/Domain/ues_cases/trip_ues_case.dart';
 import 'features/trip_feature/logic/trip_cubit/trip_cubit.dart';
 
