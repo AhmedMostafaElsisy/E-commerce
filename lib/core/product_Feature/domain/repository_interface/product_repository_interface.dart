648ee56628fd1f95e5ac66445fcd752cc286c42c
queentechdeveloper <mahmoudragab0809@gmail.com>
2023-02-17 18:12:10 +0200

    add product repo interface to use it on get general products with option of filter or any cycle needed

---
+++ lib/core/product_Feature/domain/repository_interface/product_repository_interface.dart
@@ -0,0 +1,16 @@
+import 'package:captien_omda_customer/core/Error_Handling/custom_error.dart';
+import 'package:captien_omda_customer/core/model/category_model.dart';
+import 'package:captien_omda_customer/core/model/product_model.dart';
+import 'package:captien_omda_customer/core/tags_feature/domain/model/tags_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class GeneralProductRepositoryInterface {
+  Future<Either<CustomError, List<ProductModel>>> getProductWithOptionParams({
+    required int page,
+    int limit = 10,
+    List<CategoryModel>? categories,
+    List<TagsModel>? tags,
+    String? cityId,
+    String? areaId,
+  });
+}
