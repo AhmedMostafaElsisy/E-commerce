bdd60e1742040c14954be9388031a024ea2b12ff
queentechdeveloper <mahmoudragab0809@gmail.com>
2023-03-01 01:37:01 +0200

    add cart model on core models

---
+++ lib/core/model/cart_model.dart
@@ -0,0 +1,51 @@
+import 'package:captien_omda_customer/core/model/product_model.dart';
+
+import '../../features/cart_feature/domain/entity/cart_entity.dart';
+
+List<CartModel> cartsListFromJson(List str) =>
+    List<CartModel>.from(str.map((x) => CartModel.fromJson(x)));
+
+class CartModel extends CartEntity {
+  CartModel({
+    required int id,
+    required num total,
+    required num price,
+    required int quantity,
+    required ProductModel productModel,
+  }) : super(
+          id: id,
+          total: total,
+          price: price,
+          quantity: quantity,
+          productModel: productModel,
+        );
+
+  factory CartModel.fromJson(Map<String, dynamic> jsonObject) {
+    try {
+      return CartModel(
+        id: jsonObject["id"],
+        total: jsonObject["total"],
+        price: jsonObject["price"],
+        quantity: jsonObject["qnt"],
+        productModel: ProductModel.fromJson(jsonObject["product"]),
+      );
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "id": id,
+      "total": total,
+      "price": price,
+      "qnt": quantity,
+      "product": productModel,
+    };
+  }
+
+  @override
+  String toString() {
+    return "cart model id: $id,total: $total, price: $price, quantity: $quantity, product: $productModel";
+  }
+}
