b5a9ab37cc5e86c5793da4c5a7ccf0c65efe63c0
queentechdeveloper <mahmoudragab0809@gmail.com>
2023-03-02 00:29:15 +0200

    add store navigation in all places

--- lib/core/presentation/Widgets/shop_grid_item_widget.dart
+++ lib/core/presentation/Widgets/shop_grid_item_widget.dart
@@ -6,6 +6,8 @@ import '../../../core/Constants/app_constants.dart';
 import '../../../core/Helpers/shared.dart';
 import '../../../core/presentation/Widgets/common_title_text.dart';
 import '../../model/shop_model.dart';
+import '../Routes/route_argument_model.dart';
+import '../Routes/route_names.dart';
 
 class ShopGridItemWidget extends StatelessWidget {
   final ShopModel model;
@@ -26,69 +28,44 @@ class ShopGridItemWidget extends StatelessWidget {
                 offset: const Offset(0, 0),
                 blurRadius: 4)
           ]),
-      child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-        ///shop image
-        commonCachedImageWidget(model.image!,
-            height: 150, width: 168, fit: BoxFit.fill),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).pushNamed(RouteNames.storeDetailsScreen,
+              arguments: RouteArgument(shopModel: model));
+        },
+        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+          ///shop image
+          commonCachedImageWidget(model.image!,
+              height: 150, width: 168, fit: BoxFit.fill),
 
-        ///spacer
-        getSpaceHeight(8),
+          ///spacer
+          getSpaceHeight(8),
 
-        ///shop name
-        CommonTitleText(
-          textKey: model.name!,
-          textFontSize: AppConstants.smallFontSize,
-          textColor: AppConstants.mainTextColor,
-          textWeight: FontWeight.w600,
-        ),
-
-        ///Spacer
-        getSpaceHeight(8),
-        Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 4.0,
-            vertical: 4,
+          ///shop name
+          CommonTitleText(
+            textKey: model.name!,
+            textFontSize: AppConstants.smallFontSize,
+            textColor: AppConstants.mainTextColor,
+            textWeight: FontWeight.w600,
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              ///shop category
-              Expanded(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const CommonAssetSvgImageWidget(
-                      imageString: "category.svg",
-                      height: 16,
-                      width: 16,
-                      fit: BoxFit.contain,
-                      imageColor: AppConstants.mainColor,
-                    ),
-                    getSpaceWidth(4),
-                    Expanded(
-                      child: CommonTitleText(
-                        textKey: model.category!.name,
-                        textFontSize: AppConstants.xxSmallFontSize,
-                        textColor: AppConstants.lightContentColor,
-                        textWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
 
-              ///shop location
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 4.0,
-                    vertical: 4,
-                  ),
+          ///Spacer
+          getSpaceHeight(8),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 4.0,
+              vertical: 4,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ///shop category
+                Expanded(
                   child: Row(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       const CommonAssetSvgImageWidget(
-                        imageString: "location.svg",
+                        imageString: "category.svg",
                         height: 16,
                         width: 16,
                         fit: BoxFit.contain,
@@ -97,7 +74,7 @@ class ShopGridItemWidget extends StatelessWidget {
                       getSpaceWidth(4),
                       Expanded(
                         child: CommonTitleText(
-                          textKey: model.address ?? "asda",
+                          textKey: model.category!.name,
                           textFontSize: AppConstants.xxSmallFontSize,
                           textColor: AppConstants.lightContentColor,
                           textWeight: FontWeight.w400,
@@ -106,11 +83,42 @@ class ShopGridItemWidget extends StatelessWidget {
                     ],
                   ),
                 ),
-              ),
-            ],
+
+                ///shop location
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 4.0,
+                      vertical: 4,
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const CommonAssetSvgImageWidget(
+                          imageString: "location.svg",
+                          height: 16,
+                          width: 16,
+                          fit: BoxFit.contain,
+                          imageColor: AppConstants.mainColor,
+                        ),
+                        getSpaceWidth(4),
+                        Expanded(
+                          child: CommonTitleText(
+                            textKey: model.address ?? "asda",
+                            textFontSize: AppConstants.xxSmallFontSize,
+                            textColor: AppConstants.lightContentColor,
+                            textWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-      ]),
+        ]),
+      ),
     );
   }
 }
